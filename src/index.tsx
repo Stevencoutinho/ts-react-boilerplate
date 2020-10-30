9c432004b42fa75d1ba4521a6c8926ed9929d8ff
ISHIBASHI <58579231+Stevencoutinho@users.noreply.github.com>
2020-10-30 10:04:05 +0900

    importパスを修正

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from '@/components/App';
-import '@/css/destyle.css';
-import '@/css/style.scss';
+import App from '@/src/components/App';
+import '@/src/css/destyle.css';
+import '@/src/css/style.scss';
 
 const rootEl = document.getElementById('root');
 
